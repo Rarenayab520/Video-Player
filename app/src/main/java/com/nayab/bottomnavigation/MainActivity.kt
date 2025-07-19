bf0af7244f5ba794365ac040673afec7dc18d652
Rarenayab520 <nayabasmat121@gmail.com>
2025-07-19 20:54:28 +0100

    drawer_layout

--- app/src/main/java/com/nayab/bottomnavigation/MainActivity.kt
+++ app/src/main/java/com/nayab/bottomnavigation/MainActivity.kt
@@ -6,21 +6,41 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.nayab.bottomnavigation.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var appBarConfiguration: AppBarConfiguration
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        // Set toolbar
+        setSupportActionBar(binding.toolbar)
 
         val navHostFragment=supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController=navHostFragment.navController
         val bottomNavigation=binding.bottomNavigationView
 
         NavigationUI.setupWithNavController(bottomNavigation,navController)
+        appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.homefragment,
+                R.id.signinfragment,
+                R.id.signupfragment
+            ),
+            binding.drawerLayout// DrawerLayout ID
+        )
 
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
+
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = binding.navHostFragment.getFragment<NavHostFragment>().navController
+        return NavigationUI.navigateUp(navController, appBarConfiguration) || super.onSupportNavigateUp()
     }
 }
